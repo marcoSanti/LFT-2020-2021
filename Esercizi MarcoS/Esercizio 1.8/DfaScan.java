f49776d2ea66bdcb4749386b2479cc8001848223
marco santimaria <marco.santimaria@live.it>
2021-01-23 15:26:34 +0100

    automa 1.8

--- Esercizi MarcoS/Esercizio 1.8/DfaScan.java
+++ Esercizi MarcoS/Esercizio 1.8/DfaScan.java
@@ -8,10 +8,87 @@ public class DfaScan{
 
             switch(status){
                 case 0:
-                    if(ch)
+                    if(ch == 'm') status = 1;
+                    else if(Character.isLetter(ch)) status = 6;
+                    else status = -1;
+                    break;
 
+                case 1:
+                    if(ch=='a') status = 2;
+                    else if(Character.isLetter(ch)) status = 10;
+                    else status = -1;
+                    break;
+
+                case 2:
+                    if(ch == 'r') status = 3;
+                    else if(Character.isLetter(ch)) status = 13;
+                    else status = -1;
+                    break;
+
+                case 3:
+                    if(ch=='c') status = 4;
+                    else if(Character.isLetter(ch)) status = 15;
+                    else status = -1;
+                    break;
+
+                case 4:
+                    if(Character.isLetter(ch)) status = 5;
+                    else status = -1;
+                    break;
+
+                case 5: //se sono qua e ho altre lettere da vedere ho aggiunto al nome e quindi non va bene e devo morire come automa
+                    status = -1;
+                    break;
                
+                case 6:
+                    if(ch=='a') status = 7;
+                    else status = -1;
+                    break;
 
+                case 7:
+                    if(ch=='r') status = 8;
+                    else status = -1;
+                    break;
+
+                case 8:
+                    if(ch=='c') status = 9;
+                    else status = -1;
+                    break;
+
+                case 9:
+                    if(ch=='o') status = 5;
+                    else status = -1;
+                    break;
+
+                case 10:
+                    if(ch=='r') status = 11;
+                    else status = -1;
+                    break;
+                
+                case 11:
+                    if(ch=='c') status = 12;
+                    else status = -1;
+                    break;
+
+                case 12:
+                    if(ch=='o') status = 5;
+                    else status = -1;
+                    break;
+
+                case 13:
+                    if(ch=='c') status = 14;
+                    else status = -1;
+                    break;
+
+                case 14:
+                    if(ch=='o') status = 5;
+                    else status = -1;
+                    break;
+
+                case 15:
+                    if(ch=='o') status = 5;
+                    else status = -1;
+                    break;
             }
 
 
--- Esercizi MarcoS/Esercizio 1.8/Main.java
+++ Esercizi MarcoS/Esercizio 1.8/Main.java
@@ -3,15 +3,21 @@
 public class Main {
     public static void main(String[] args){
         System.out.print("\n----Test Accettati----\n");
-        System.out.println("Stringa \"aaa\"     accettata? " + DfaScan.scan("aaa"));
-        System.out.println("Stringa \"abb\"     accettata? " + DfaScan.scan("abb"));
-        System.out.println("Stringa \"bab\"     accettata? " + DfaScan.scan("bab"));
-        System.out.println("Stringa \"bbabbba\" accettata? " + DfaScan.scan("bbabbba"));
+        System.out.println("Stringa \"marco\"     accettata? " + DfaScan.scan("marco"));
+        System.out.println("Stringa \"xarco\"     accettata? " + DfaScan.scan("xarco"));
+        System.out.println("Stringa \"mxrco\"     accettata? " + DfaScan.scan("mxrco"));
+        System.out.println("Stringa \"maxco\"     accettata? " + DfaScan.scan("maxco"));
+        System.out.println("Stringa \"marxo\"     accettata? " + DfaScan.scan("marxo"));
+        System.out.println("Stringa \"marcx\"     accettata? " + DfaScan.scan("marcx"));
+        
 
         System.out.print("\n----Test NON Accettati----\n");
-        System.out.println("Stringa \"bbbabbb\" accettata? " + DfaScan.scan("bbbabbb"));
-        System.out.println("Stringa \"\"        accettata? " + DfaScan.scan(""));
-        System.out.println("Stringa \"bbbbbbb\" accettata? " + DfaScan.scan("bbbbbbb"));
+        System.out.println("Stringa \"marcox\"     accettata? " + DfaScan.scan("marcox"));
+        System.out.println("Stringa \"xxarco\"     accettata? " + DfaScan.scan("xxarco"));
+        System.out.println("Stringa \"mxxrco\"     accettata? " + DfaScan.scan("mxxrco"));
+        System.out.println("Stringa \"maxxco\"     accettata? " + DfaScan.scan("maxxco"));
+        System.out.println("Stringa \"marxxo\"     accettata? " + DfaScan.scan("marxxo"));
+        System.out.println("Stringa \"marcxx\"     accettata? " + DfaScan.scan("marcxx"));
       
 
 
